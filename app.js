da538a5ef61db30e77660ef5f85df6ed9ded712b
florzeta <99660646+florzeta@users.noreply.github.com>
2024-03-08 22:19:07 -0300

    rutas contacto, promociones, novedades

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var contactoRouter = require('./routes/contacto');
 
 var app = express();
 
@@ -21,6 +22,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use('/contacto', contactoRouter);
+
+
+app.get('/promociones', function (req,res) {
+  res.send('En esta pagina podras ver nuestras promociones del mes')  
+})
+
+app.get('/novedades', function (req,res) {
+  res.send('En esta pagina podras ver los ingresos y novedades del mes')  
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/contacto.js
+++ routes/contacto.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hola!!' });
+  res.send('Bienveninos a la pagina de contacto');
 });
 
 module.exports = router;
